24cf12e9b7d12fede4f992256aaf99eed4c9240f
EngFred <engfred88@gmail.com>
2024-06-11 10:13:52 +0300

    last commit

--- lib/presentation/constants.dart
+++ lib/presentation/constants.dart
@@ -5,7 +5,7 @@ enum ColorSelection {
   deepPurple('Deep Purple', Colors.deepPurple),
   purple('Purple', Colors.purple),
   indigo('Indigo', Colors.indigo),
-  blue('Blue', Color.fromARGB(255, 4, 6, 7)),
+  blue('Blue', Colors.blue),
   yellow('Yellow', Colors.yellow),
   orange('Orange', Colors.orange),
   deepOrange('Deep Orange', Colors.deepOrange),
